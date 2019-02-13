91e2f2179b3af5e7629de8dd58b023c2d02525f4
praveenatla01 <praveenatla01.gmail.com>
2019-02-13 23:25:52 +0530

    Branch_1 change

--- string.c
+++ string.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 void main()
 {
+int d
 int a = 5;
 int b = 6;
-c = a+b;
-a = c-a;
-b = c-b;
+c = a+b; //this change is for branch1
+a = c-a; //this change is for branch1
+d = c-b;
 printf("%d", c);
 }
