fc2a73925a51ddb3ff590c7db9d990ebcc8596a4
praveenatla01 <praveenatla01.gmail.com>
2019-02-14 00:10:30 +0530

    for creating the conflict 1

--- string.c
+++ string.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void main()
 {
-int d
+int d;
 int a = 5;
 int b = 6;
 c = a+b; //this change is for branch1
 a = c-a; //this change is for branch1
-d = c-b;
+d = c-b; //to check the merge conflict
 printf("%d", c);
 }
